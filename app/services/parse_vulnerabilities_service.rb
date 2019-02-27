5bdc864cd2024edd44150a5d9fe159306311f95d
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-02-27 23:40:06 +0200

    add parse_vulnerabilities_service + refactor rubysec file

---
+++ app/services/parse_vulnerabilities_service.rb
@@ -0,0 +1,24 @@
+class ParseVulnerabilitiesService
+  attr_reader :td_elements
+
+  def initialize
+    @td_elements = Rubysec.new.call
+  end
+
+  def call
+    parse_vulnerabilities
+  end
+
+  private
+
+  def parse_vulnerabilities
+    td_elements.in_groups_of(4).each do |vul_elems|
+      Vulnerability.create(
+        published_at: vul_elems[0].to_date,
+        rubygem: vul_elems[1],
+        title: vul_elems[2],
+        cve: vul_elems[3],
+      )
+    end
+  end
+end
--- lib/rubysec.rb
+++ lib/rubysec.rb
@@ -5,41 +5,13 @@ class Rubysec
   URL = 'https://rubysec.com/advisories'.freeze
   TD = 'td'.freeze
 
-  attr_reader :published_at, :rubygem, :title, :cve
-
-  def initialize
-    @published_at = []
-    @rubygem = []
-    @title = []
-    @cve = []
-  end
-
   def call
-    parse_fields
-    {
-      published_at: published_at,
-      rubygem: rubygem,
-      title: title,
-      cve: cve
-    }
+    fetch_page.css(TD).map(&:text).map(&:strip)
   end
 
   private
 
   def fetch_page
     Nokogiri::HTML(open(URL, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
-  end
-
-  def td_elements
-    fetch_page.css(TD).map(&:text).map(&:strip)
-  end
-
-  def parse_fields
-    td_elements.in_groups_of(4).each do |vul_elems|
-      @published_at << vul_elems[0]
-      @rubygem << vul_elems[1]
-      @title << vul_elems[2]
-      @cve << vul_elems[3]
-    end
   end
 end
